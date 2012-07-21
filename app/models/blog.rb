7091910bbc5d595d3c118a4ace3b9f336d3b6db9
Shabda Raaj <shabda@agiliq.com>
2012-07-21 22:33:28 +0530

    Added vaidations for blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ActiveRecord::Base
   attr_accessible :post, :slug, :title, :post_type, :summary
+  validates :post, :presence => true
+  validates :slug, :presence => true
+  validates :title, :presence => true
+  validates :post_type, :presence => true
+  validates :summary, :presence => true  
 end
