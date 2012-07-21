d1b308ba61fb4106af6abe76ba4e91e41bab5a21
Shabda Raaj <shabda@agiliq.com>
2012-07-21 22:53:01 +0530

    Write a before_save callback to populate auto fields

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,10 @@
 class Blog < ActiveRecord::Base
   attr_accessible :post, :slug, :title, :post_type, :summary
   validates :post, :presence => true
-  validates :slug, :presence => true
   validates :title, :presence => true
-  validates :post_type, :presence => true
-  validates :summary, :presence => true  
+  validates :post_type, :presence => true  
+  before_save do |record|
+    record.slug ||= record.title.split.join("-")
+    record.summary ||= record.post.split[0...10].join(" ")
+  end
 end
