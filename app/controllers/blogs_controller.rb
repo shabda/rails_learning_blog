a89c0f5e714e936593aea3a419aec8ec5fd4c440
Shabda Raaj <shabda@agiliq.com>
2012-07-22 14:38:12 +0530

    Remove redundant comments.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,31 +1,24 @@
 class BlogsController < ApplicationController
-  # GET /blogs
-  # GET /blogs.json
+  
   respond_to :html
   
   def index
     @blogs = Blog.find(:all, :conditions => {:post_type=>true})
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
     @blog = Blog.find(params[:id])
   end
 
-  # GET /blogs/new
-  # GET /blogs/new.json
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
+
   def edit
     @blog = Blog.find(params[:id])
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(params[:blog])
     @blog.post_type = true
@@ -37,8 +30,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PUT /blogs/1
-  # PUT /blogs/1.json
   def update
     @blog = Blog.find(params[:id])
     if @blog.update_attributes(params[:blog])
@@ -48,11 +39,10 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog = Blog.find(params[:id])
     @blog.destroy
     redirect_to blogs_url
   end
+
 end
