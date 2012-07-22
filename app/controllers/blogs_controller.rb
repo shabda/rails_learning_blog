14f3e905dc2d5b181e689bb3eb162290370560bb
Shabda Raaj <shabda@agiliq.com>
2012-07-22 09:52:17 +0530

    Removed the JSON boilerplate

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,35 +1,22 @@
 class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
+  respond_to :html
+  
   def index
     @blogs = Blog.find(:all, :conditions => {:post_type=>true})
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @blogs }
-    end
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
     @blog = Blog.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @blog }
-    end
   end
 
   # GET /blogs/new
   # GET /blogs/new.json
   def new
     @blog = Blog.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @blog }
-    end
   end
 
   # GET /blogs/1/edit
@@ -46,10 +33,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render json: @blog, status: :created, location: @blog }
       else
         format.html { render action: "new" }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,10 +47,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update_attributes(params[:blog])
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -78,7 +61,6 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to blogs_url }
-      format.json { head :no_content }
     end
   end
 end
